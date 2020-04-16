0cf89f5010fa49480519a5985231f8f350ed638c
olena_rolskaya <manitskayaelena@gmail.com>
2020-04-16 13:43:40 +0300

    store created commit

---
+++ src/data/products.js
@@ -0,0 +1,23 @@
+const items = [
+  {
+    sku: "sku_H6gYJj5EjeP3gq", 
+    quantity: 1, 
+    price: 17900, 
+    name: "D-Link DES-1005C"
+  },
+  {
+    sku: "sku_H6gYJj5EjeP3gq", 
+    quantity: 1, 
+    price: 17900, 
+    name: "D-Link DES-1005C"
+  },
+  {
+    sku: "sku_H6gYJj5EjeP3gq", 
+    quantity: 1, 
+    price: 17900, 
+    name: "D-Link DES-1005C"
+  }
+]
+
+
+export default items
