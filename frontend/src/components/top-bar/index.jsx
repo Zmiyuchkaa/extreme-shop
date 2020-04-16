98ca60a1f0cd493f19c6df29317c94e48f7db2c8
olena_rolskaya <manitskayaelena@gmail.com>
2020-04-16 19:50:52 +0300

    login styling finished

--- frontend/src/components/top-bar/index.jsx
+++ frontend/src/components/top-bar/index.jsx
@@ -2,14 +2,13 @@ import React, { useContext } from "react";
 import {Link as RouterLink} from "react-router-dom";
 import "./styles.css";
 import {CartContext} from "../cart/context";
-import LogIn from "../../pages/login"
 
 export default function TopBar() {
   const cartCtx = useContext(CartContext);
   const numItems = cartCtx.itemsCount;
   return <div className="layout top-bar">
     <div className="wrapper">
-      <div className="section">Логотип</div>
+      <div className="section"><h3 className="title">Логотип</h3></div>
       <div className="section">
         <RouterLink to="/">Войти в аккаунт</RouterLink>
       </div>
--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -1,29 +1,45 @@
-import React, {useState} from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import { useCookies } from 'react-cookie';
+import React, {useContext, useState } from "react";
+import {Link as RouterLink} from "react-router-dom";
+import "./styles.css";
+import {CartContext} from "../../components/cart/context";
 
 
 
 export default function LogIn() {
+  const cartCtx = useContext(CartContext);
+  const numItems = cartCtx.itemsCount;
+
   return (
     <div>
-      <label><b>Логин</b></label>
-      <input 
-        type="text" 
-        placeholder="Enter Username"  
-        autoFocus 
-        name="login" 
-        required
-      />
-      <label><b>Пароль</b></label>
-      <input 
-        type="text" 
-        placeholder="Enter Username" 
-        name="login" 
-        required
-      />
-      <button><Link to="/products">Войти</Link></button>
+      <div className="layout top-bar">
+        <div className="wrapper">
+          <div className="section"><h3 className="title">Логотип</h3></div>
+          <div className="section">
+            <RouterLink className="link" to="/">Войти в аккаунт</RouterLink>
+          </div>
+      </div>
+      </div>
+      <div className="login-form">
+        <label className="label"><b>Логин</b></label>
+        <input 
+          className="input-field"
+          type="text" 
+          placeholder="Введите Email"  
+          autoFocus 
+          name="login" 
+          required
+        />
+        <label className="label"><b>Пароль</b></label>
+        <input 
+          className="input-field"
+          type="text" 
+          placeholder="Введите пароль" 
+          name="login" 
+          required
+        />
+        <button className="button"><RouterLink className="link" to="/products">Войти</RouterLink></button>
+      </div>
     </div>
+    
   );
 }
